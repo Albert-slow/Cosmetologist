901540347cf4f8973be79bf51c16495bdc2a636a
Albert-slow <142785734+Albert-slow@users.noreply.github.com>
2024-03-28 12:57:45 +0500

    Update settings.py
    deleted secret_key

--- CosmetologySite/settings.py
+++ CosmetologySite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g4d9aow74+tc((!7ctjt_37jat)v(122isyt&za7=@@zez+$b='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
